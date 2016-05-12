4d0e41ce0eec255ead0e3a7f119512522641ea4d
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-05-12 08:55:57 +0100

    feat: User can get results for synonyms and antonyms (#2)
    User can get results for synonyms and antonyms
    - Add method to return unique results from an array
    - Add method for showing synonyms and antonyms

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,8 +1,3 @@
 (function () {
-  // document.body.innerHTML = "HAHAHAHAHA!!";
-  // console.log("Dictionary");
-
-  document.addEventListener("click", function (e) {
-    console.log(e.target);
-  }, false);
+  
 }());
